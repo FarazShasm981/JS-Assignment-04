68ce6c91e2d0ee87f0308eebe99e3e98707de968
FarazShasm981 <hafizfaraz981@gmail.com>
2025-09-14 16:01:11 +0500

    Six Questions

---
+++ app.js
@@ -0,0 +1,67 @@
+// ! CHAPTER # 17-20
+
+// Question #01 Start
+// var multArr = [[],[],[]];
+// Question #01 End
+
+// Question #02 Start
+// var matrix = [
+//     [0,1,2,3],
+//     [1,0,1,2],
+//     [2,1,0,1]
+// ]
+// document.write(matrix[0].join(" ") + "<br>" + matrix[1].join(" ") + "<br>" + matrix[2].join(" "));
+// Question #02 End
+
+// Question #03 Start
+// for (let i = 1; i <= 10; i++) {
+//   document.write(i + "<br>");
+// }
+// Question #03 End
+
+// Question #04 Start
+// var userInp = parseInt(prompt("Enter a number"));
+// var length = parseInt(prompt("Enter length of table"));
+// for (var i = 1; i <= length; i++) {
+//   document.write(`${userInp} x ${i} = ${userInp * i} <br>`);
+// }
+// Question #04 End
+
+// Question #05 Start
+// var fruitsArr = ["apple", "banana", "mango", "orange", "strawberry"];
+// for (var i = 0; i < fruitsArr.length; i++) {
+//   document.write(`Element at index ${i} is ${fruitsArr[i]}<br>`);
+// }
+// Question #05 End
+
+// Question #06 Start
+// document.write("Counting : ");
+// for (var i = 1; i <= 15; i++) {
+//   document.write(i + ",");
+// }
+// document.write("<br>Reverse Counting : ");
+// for (var i = 10; i >= 1; i--) {
+//   document.write(i + ",");
+// }
+// document.write("<br>Even Number : ");
+// for (var i = 0; i <= 20; i++) {
+//     //   document.write(i + ",");
+//     if (i % 2 === 0) {
+//         document.write(i + ",");
+//     }
+// }
+// document.write("<br>Odd Number : ");
+// for (var i = 0; i <= 20; i++) {
+//     //   document.write(i + ",");
+//     if (i % 2 != 0) {
+//         document.write(i + ",");
+//     }
+// }
+// document.write("<br>Series : ");
+// for (var i = 1; i <= 20; i++) {
+//     //   document.write(i + ",");
+//     if (i % 2 === 0) {
+//         document.write(i + "k ");
+//     }
+// }
+// Question #06 End
